b07eb60723563f576b0f70fbd0e950c61d8c8641
liyy <ants_double@yeah.net>
2019-10-22 11:08:03 +0800

    type util

--- utils/lib/AntsStringUtil.js
+++ utils/lib/AntsStringUtil.js
@@ -8,4 +8,62 @@ module.exports = class AntsStringUtil {
     static removeDuplicate(str) {
         return [...new Set(str)].join('');
     }
+
+    /*
+        检测密码强度
+    */
+    static checkPwd(str) {
+        var Lv = 0;
+        if (str.length < 6) {
+            return Lv
+        }
+        if (/[0-9]/.test(str)) {
+            Lv++
+        }
+        if (/[a-z]/.test(str)) {
+            Lv++
+        }
+        if (/[A-Z]/.test(str)) {
+            Lv++
+        }
+        if (/[\.|-|_]/.test(str)) {
+            Lv++
+        }
+        return Lv;
+    }
+
+    /**
+    * @param  {str} 
+    * @param  {type}
+    *       type:  1:首字母大写  2：首页母小写  3：大小写转换  4：全部大写  5：全部小写
+    * @return {String}
+    */
+    static changeCase(str, type) {
+        type = type || 4
+        switch (type) {
+            case 1:
+                return str.replace(/\b\w+\b/g, function (word) {
+                    return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+
+                });
+            case 2:
+                return str.replace(/\b\w+\b/g, function (word) {
+                    return word.substring(0, 1).toLowerCase() + word.substring(1).toUpperCase();
+                });
+            case 3:
+                return str.split('').map(function (word) {
+                    if (/[a-z]/.test(word)) {
+                        return word.toUpperCase();
+                    } else {
+                        return word.toLowerCase()
+                    }
+                }).join('')
+            case 4:
+                return str.toUpperCase();
+            case 5:
+                return str.toLowerCase();
+            default:
+                return str;
+        }
+    }
 }
